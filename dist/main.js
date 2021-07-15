9c6faec59c0d38a22e42072e9e19056c67bd8161
duyettwyler2k <duyettwyller@gmail.com>
2021-07-15 15:24:58 +0700

    'asd'

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const rectangle_1 = require("./rectangle");
+const rectangle = new rectangle_1.Rectangle(4, 2, 4, 5);
+console.log(rectangle.toString());
+console.log(rectangle.area());
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import {Rectangle} from "./rectangle";
+const rectangle=new Rectangle(4,2,4,5);
+console.log(rectangle.toString())
+console.log(rectangle.area())
